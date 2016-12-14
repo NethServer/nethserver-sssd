c7e46b71f3937b007750f333ad5710a09078fb61
Davide Principi <davide.principi@nethesis.it>
2016-12-14 11:38:45 +0100

    Disable form if LDAP local provider
    The AD local provider case is covered by the previous commit

--- root/usr/share/nethesis/NethServer/Template/SssdConfig/AuthProvider/Index.php
+++ root/usr/share/nethesis/NethServer/Template/SssdConfig/AuthProvider/Index.php
@@ -8,28 +8,31 @@ if($view['Provider'] === 'ad') {
     $ldapEnabled = $view::STATE_DISABLED;
 } elseif($view['Provider'] === 'ldap') {
     $adEnabled = $view::STATE_DISABLED;
+    if($view['RawLdapUri'] === 'ldap://127.0.0.1') {
+        $globalFlags = $view::STATE_DISABLED | $view::STATE_READONLY;
+    }
 }
 
 echo $view->panel()
-        ->insert($view->radioButton('Provider', 'none', 0))
-        ->insert($view->fieldsetSwitch('Provider', 'ldap', $view::FIELDSET_EXPANDABLE | $ldapEnabled)
+        ->insert($view->radioButton('Provider', 'none', $globalFlags))
+        ->insert($view->fieldsetSwitch('Provider', 'ldap', $view::FIELDSET_EXPANDABLE | $ldapEnabled | $globalFlags)
                 ->insert($view->textInput('LdapUri')->setAttribute('placeholder', $view['defaultLdapUri']))
         )
-        ->insert($view->fieldsetSwitch('Provider', 'ad', $view::FIELDSET_EXPANDABLE | $adEnabled)
+        ->insert($view->fieldsetSwitch('Provider', 'ad', $view::FIELDSET_EXPANDABLE | $adEnabled | $globalFlags )
                 ->insert($view->textInput('NetbiosDomain', $view::STATE_DISABLED | $view::STATE_READONLY))
-                ->insert($view->textInput('AdDns')->setAttribute('placeholder', $view['defaultAdDns']))
+                ->insert($view->textInput('AdDns', $globalFlags)->setAttribute('placeholder', $view['defaultAdDns']))
         )
 ;
 
 
-echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('SssdConfig_Advanced_label'))
-    ->insert($view->textInput('BindDN')->setAttribute('placeholder', $view['sssd_defaults']['bindDN']))
-    ->insert($view->textInput('BindPassword')->setAttribute('placeholder', $view['sssd_defaults']['bindPassword']))
-    ->insert($view->textInput('BaseDN')->setAttribute('placeholder', $view['sssd_defaults']['baseDN']))
-    ->insert($view->textInput('UserDN')->setAttribute('placeholder', $view['sssd_defaults']['userDN']))
-    ->insert($view->textInput('GroupDN')->setAttribute('placeholder', $view['sssd_defaults']['groupDN']))
-    ->insert($view->textInput('RawLdapUri')->setAttribute('placeholder', $view['sssd_defaults']['ldapURI']))
-    ->insert($view->selector('StartTls', $view::SELECTOR_DROPDOWN)->setAttribute('choices', \Nethgui\Widget\XhtmlWidget::hashToDatasource(array(
+echo $view->fieldset('', $view::FIELDSET_EXPANDABLE | $globalFlags )->setAttribute('template', $T('SssdConfig_Advanced_label'))
+    ->insert($view->textInput('BindDN', $globalFlags)->setAttribute('placeholder', $view['sssd_defaults']['bindDN']))
+    ->insert($view->textInput('BindPassword', $globalFlags)->setAttribute('placeholder', $view['sssd_defaults']['bindPassword']))
+    ->insert($view->textInput('BaseDN', $globalFlags)->setAttribute('placeholder', $view['sssd_defaults']['baseDN']))
+    ->insert($view->textInput('UserDN', $globalFlags)->setAttribute('placeholder', $view['sssd_defaults']['userDN']))
+    ->insert($view->textInput('GroupDN', $globalFlags)->setAttribute('placeholder', $view['sssd_defaults']['groupDN']))
+    ->insert($view->textInput('RawLdapUri', $globalFlags)->setAttribute('placeholder', $view['sssd_defaults']['ldapURI']))
+    ->insert($view->selector('StartTls', $globalFlags | $view::SELECTOR_DROPDOWN)->setAttribute('choices', \Nethgui\Widget\XhtmlWidget::hashToDatasource(array(
         '' => $T('starttls_auto'),
         'enabled' => $T('starttls_enabled'),
         'disabled' => $T('starttls_disabled')))))
@@ -37,4 +40,4 @@ echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $
 
 
 echo $view->buttonList($view::BUTTON_HELP)
-        ->insert($view->button('Submit', $view::BUTTON_SUBMIT));
+        ->insert($view->button('Submit', $view::BUTTON_SUBMIT | $globalFlags));
