ec645626cf6ea7d5873bddd1e9cc360069e84463
dnutan <dnutan@users.noreply.github.com>
2017-07-09 10:44:17 +0200

    DC: allow changing nsdc IP address

--- root/usr/share/nethesis/NethServer/Template/SssdConfig/LocalAdProvider.php
+++ root/usr/share/nethesis/NethServer/Template/SssdConfig/LocalAdProvider.php
@@ -8,9 +8,21 @@ echo $view->header('domain')->setAttribute('template', $T('LocalAdProvider_heade
 
 echo $view->textInput('AdRealm', $disabledFlags);
 echo $view->textInput('AdWorkgroup', $disabledFlags);
-echo $view->textInput('AdIpAddress', $disabledFlags);
 
-$buttons = $view->buttonList($view::BUTTON_HELP)
+$AdIpAddressId = $view->getUniqueId('AdIpAddress');
+$labelOpenTag = "<label for='$AdIpAddressId'>";
+$help = '<div class="dcalert notification bg-yellow">
+  <p>' . $labelOpenTag . '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> ' . htmlspecialchars($T('AdIpAddress_help1')) . '</label></p>
+  <ul>
+    <li>' . $labelOpenTag . $view->textLabel('greenList')->setAttribute('template', $T('AdIpAddress_help2')) . '</label></li>
+    <li>' . $labelOpenTag . htmlspecialchars($T('AdIpAddress_help3')) . '</label></li>
+  </ul>
+</div>';
+
+echo $view->textInput('AdIpAddress');
+echo $help;
+
+$buttons = $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP)
         ->insert($view->button('LocalAdProviderUninstall', $view::BUTTON_LINK));
 
 if($view->getModule()->isSambaUpdateAvailable()) {
@@ -18,3 +30,33 @@ if($view->getModule()->isSambaUpdateAvailable()) {
 }
 
 echo $buttons;
+
+
+
+$view->includeCss("
+.dcalert {
+    color: #000;
+    background-color: #FFB600;
+    border: 1px solid #FFB600;
+    border-radius: 2px;
+    padding: 15px;
+    margin: 10px;
+    position: relative;
+}
+.dcalert:before {
+  content: '';
+  position: absolute;
+  bottom: 100%;
+  left: 20px;
+  width: 0;
+  border-bottom: 18px solid #FFB600;
+  border-left: 18px solid transparent;
+  border-right: 18px solid transparent;
+}
+.notification.bg-yellow {color: #000; background-color: #FFB600; border-color: #FFB600 }
+.notification.bg-yellow a {color: #000}
+.dcalert ul {
+    list-style-type: disc;
+    margin-left: 25px;
+}
+");
