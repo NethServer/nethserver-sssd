1dc9c0ab8e8e109ce849ca0a0a94ad7b242959e3
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-09-15 17:06:49 +0200

    Group alias email creation in Template

--- root/usr/share/nethesis/NethServer/Template/Group/Modify.php
+++ root/usr/share/nethesis/NethServer/Template/Group/Modify.php
@@ -8,12 +8,13 @@ if ($view->getModule()->getIdentifier() == 'update') {
     $headerText = $T('Create a new group');
     $groupname = (string) $view->textInput('groupname');
     $groupname = str_replace("</div>", "@".$view['domain']."</div>", $groupname);
+    $pseudonymCreation = 'yes';
 }
 
 echo $view->header('groupname')->setAttribute('template', $headerText);
 
 $groupInfo = $view->panel()
-    ->setAttribute('title', $T('GroupTab_Title'))    
+    ->setAttribute('title', $T('GroupTab_Title'))
     ->insert($view->literal($groupname))
     ->insert($view->objectPicker('members')
         ->setAttribute('objects', 'membersDatasource')
@@ -22,5 +23,10 @@ $groupInfo = $view->panel()
 
 echo $groupInfo;
 
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
+//display only on group creation
+if ($pseudonymCreation === 'yes' and
+        (file_exists ('/etc/e-smith/db/configuration/defaults/dovecot/status'))) {
+    echo $view->fieldset()->setAttribute('template', $T('ExtraFields_label'))->insert($view->checkBox('CreatePseudoRecords', 'yes')->setAttribute('uncheckedValue', 'no'));
+}
 
+echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_CANCEL | $view::BUTTON_HELP);
